223c830047956ea1ba46d210f7edbabd8a2f5465
João Pedro Aguiar Santos <jotapedrok@gmail.com>
2021-10-26 19:26:53 -0300

    requisito 7

--- src/services/api.js
+++ src/services/api.js
@@ -5,27 +5,7 @@ export async function getCategories() {
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
-  const categoryAndQuery = categoryId && query;
-  if (categoryAndQuery) {
-    const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
-    const result = await response.json();
-    return result;
-  }
-  const onlyCategory = categoryId && !query;
-  if (onlyCategory) {
-    const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`);
-    const result = await response.json();
-    return result;
-  }
-  const onlyQuery = !categoryId && query;
-  if (onlyQuery) {
-    const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`);
-    const result = await response.json();
-    return result;
-  }
-  return (
-    {
-      results: [],
-    }
-  );
+  const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
+  const result = await response.json();
+  return result;
 }
